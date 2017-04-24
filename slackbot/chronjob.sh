ccf827b5b69899d9a9e8619d44240cec6b157b6b
matthewpnelson <matthewpeternelson@gmail.com>
2017-04-23 23:51:46 -0600

    working

--- slackbot/chronjob.sh
+++ slackbot/chronjob.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [ -f /home/w205/w205_Final_Project_Complete/slackbot/sfhomebot.txt ]; then
+    su - w205
     cd /data/spark15/bin/
     ./spark-submit /home/w205/w205_Final_Project_Complete/evaluation1/run_evaluation.py
+    sleep 3m
     rm /home/w205/w205_Final_Project_Complete/slackbot/sfhomebot.txt
 fi
