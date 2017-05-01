378c65068966c967e294bd4bcb1fa9d13f81d714
Amy <amys@berkeley.edu>
2017-05-01 00:41:34 -0600

    cron

--- slackbot/chronjob3.sh
+++ slackbot/chronjob3.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-# Set up PATH variables for SPARK
-export SPARK=/data/spark15
-export SPARK_HOME=$SPARK
-export PATH=$SPARK/bin:$PATH
 
 if [ -f /home/w205/w205_Final_Project_Complete/slackbot/sfhomebot.txt ]; then
     #cd /data/spark15/ &&
