f87fd037f08c5aada6488c17bd7cd25258ba1c86
Amy <amys@berkeley.edu>
2017-05-01 00:43:45 -0600

    cron

--- slackbot/chronjob3.sh
+++ slackbot/chronjob3.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 if [ -f /home/w205/w205_Final_Project_Complete/slackbot/sfhomebot.txt ]; then
-    #cd /data/spark15/ &&
     spark-submit /home/w205/w205_Final_Project_Complete/evaluation1/run_evaluation.py
 fi
